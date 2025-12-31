f114fe6b529df35d9007a343a5a42c9c3c7efb91
Morgan Courgnaud <morgan@Morgans-MacBook-Air-2.local>
2025-12-30 23:26:39 -0500

    Fix tab title

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>49 Generator</title>
+      <meta name="description" content="Generate a memo Word document from a template." />
+      <meta name="application-name" content="Memo Generator" />
+      <meta name="apple-mobile-web-app-title" content="Memo Generator" />
+    </>
+  );
+}
