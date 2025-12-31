04ed0342a7aab2f037907f4fb9231e0437c842fb
Morgan Courgnaud <morgan@Morgans-MacBook-Air-2.local>
2025-12-31 14:08:32 -0500

    Set favicon to custom SVG

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,8 @@ export default function Head() {
     <>
       <title>49 Generator</title>
       <meta name="description" content="Generate a memo Word document from a template." />
-      <meta name="application-name" content="Memo Generator" />
-      <meta name="apple-mobile-web-app-title" content="Memo Generator" />
+      <meta name="application-name" content="49 Generator" />
+      <meta name="apple-mobile-web-app-title" content="49 Generator" />
     </>
   );
 }
