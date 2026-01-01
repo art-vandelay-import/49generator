5ff7f8d145dbd3a0fe6c8dd5b74501eefae9414a
Morgan Courgnaud <morgan@Morgans-MacBook-Air-2.local>
2026-01-01 16:44:24 -0500

    Update template/header

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <title>49 Generator</title>
-      <meta name="description" content="Generate a 49 in Word." />
+      <meta name="description" content="Automatically generate a 49 in Word." />
       <meta name="application-name" content="49 Generator" />
       <meta name="apple-mobile-web-app-title" content="49 Generator" />
     </>
